971edcf42084134cc1787a6d5b4e267a340bcd37
eloranta <hannu.eloranta@gmail.com>
2025-07-05 11:22:39 +0300

    add checkboxes drawing

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,54 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QSqlQuery>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    MyModel  *model = new MyModel;
+
+    ui->tableView->setModel(model);
+    model->setTable("my_table");
+
+
+    ui->tableView->setItemDelegate(new CheckboxDelegate(ui->tableView));
+
+    QSqlQuery query;
+
+    QString q = R"(
+    CREATE TABLE IF NOT EXISTS my_table (
+        call TEXT,
+        "10" TEXT,
+        "12" TEXT,
+        "15" TEXT,
+        "17" TEXT,
+        "20" TEXT,
+        "30" TEXT,
+        "40" TEXT,
+        "80" TEXT
+        )
+    )";
+
+    if (!query.exec(q))
+    {
+        qDebug() << "Cannot create table";
+        return;
+    }
+
+    query.prepare(R"(INSERT INTO my_table (call, "10", "12") VALUES (?, ?, ?))");
+    query.addBindValue("OH2LHE");
+    query.addBindValue("CP84");
+    query.addBindValue("CP84");
+
+    if (!query.exec()) {
+        qDebug() << "Insert failed!";
+    } else {
+        qDebug() << "Insert OK!";
+    }
+
+    model->select();
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,91 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStyledItemDelegate>
+#include <QApplication>
+#include <QMouseEvent>
+#include <QSqlTableModel>
+
+class MyModel : public QSqlTableModel
+{
+public:
+    MyModel()
+    {
+        cellData = false;
+     }
+    QVariant data(const QModelIndex &index, int role) const
+    {
+        if (role == Qt::UserRole) {
+            return QVariant::fromValue(cellData);  //.at(index.row()).at(index.column()));
+        }
+        return QVariant();
+    }
+
+    bool setData(const QModelIndex &index, const QVariant &value, int role)
+    {
+        if (role == Qt::UserRole) {
+            cellData = value.toBool();
+            emit dataChanged(index, index);
+            qDebug() << "there";
+
+            return true;
+        }
+        return false;
+    }
+    bool cellData;
+};
+
+
+
+class CheckboxDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+
+public:
+    using QStyledItemDelegate::QStyledItemDelegate;
+
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override
+    {
+        bool bools = index.data(Qt::UserRole).value<bool>();
+
+        int boxWidth = option.rect.width() / 4;
+
+        for (int i = 0; i < 4; ++i) {
+            QStyleOptionButton checkBoxOption;
+            checkBoxOption.state = bools ? QStyle::State_On : QStyle::State_Off;
+            checkBoxOption.rect = QRect(
+                option.rect.left() + i * boxWidth,
+                option.rect.top(),
+                boxWidth,
+                option.rect.height()
+                );
+            QApplication::style()->drawControl(QStyle::CE_CheckBox, &checkBoxOption, painter);
+        }
+    }
+
+    bool editorEvent(QEvent *event,
+                     QAbstractItemModel *model,
+                     const QStyleOptionViewItem &option,
+                     const QModelIndex &index) override
+    {
+        if (event->type() == QEvent::MouseButtonRelease) {
+            QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+
+            //int boxWidth = option.rect.width() / 4;
+            //int clickedIndex = (mouseEvent->pos().x() - option.rect.x()) / boxWidth;
+
+            bool bools = index.data(Qt::UserRole).value<bool>();
+            bools = !bools;
+            model->setData(index, QVariant::fromValue(bools), Qt::UserRole);
+            qDebug() << "here" << bools;
+         }
+
+        return true;
+    }
+};
+
+
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
