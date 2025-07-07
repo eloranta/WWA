92fde6e8a994c826f129f7d63154413fbef905df
eloranta <hannu.eloranta@gmail.com>
2025-07-07 19:33:42 +0300

    initial files

--- main.cpp
+++ main.cpp
@@ -1,25 +1,10 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QSqlDatabase>
-
-bool openDatabase(const QString& name)
-{
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName(name);
-    return db.open();
-}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    if (!openDatabase("wwa.sqlite"))
-    {
-        qDebug() << "Cannot open database";
-        return -1;
-    }
-
     MainWindow w;
     w.show();
     return a.exec();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,61 +1,11 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include <QSqlQuery>
-
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    MyModel  *model = new MyModel;
-
-    model->setRowCount(3);
-    model->setColumnCount(3);
-    ui->tableView->setModel(model);
- //   model->setTable("my_table");
-    model->setData(model->index(0, 1), 1, Qt::UserRole);  // All bits off
-    model->setData(model->index(1, 1), 1, Qt::UserRole);  // All bits off
-
-    CheckboxDelegate *delegate = new CheckboxDelegate(ui->tableView);
-    ui->tableView->setItemDelegateForColumn(1, delegate);
-    ui->tableView->setItemDelegateForColumn(2, delegate);
- //   model->select();
-
-    //ui->tableView->setColumnWidth(0, 200);
-
-    // QString q = R"(
-    // CREATE TABLE IF NOT EXISTS my_table (
-    //     call TEXT,
-    //     "10" INTEGER,
-    //     "12" INTEGER,
-    //     "15" INTEGER,
-    //     "17" INTEGER,
-    //     "20" INTEGER,
-    //     "30" INTEGER,
-    //     "40" INTEGER,
-    //     "80" INTEGER
-    //     )
-    // )";
-
-    // if (!query.exec(q))
-    // {
-    //     qDebug() << "Cannot create table";
-    //     return;
-    // }
-
-    // query.prepare(R"(INSERT INTO my_table (call, "10", "12") VALUES (?, ?, ?))");
-    // query.addBindValue("OH2LHE");
-    // query.addBindValue(1);
-    // query.addBindValue(15);
-
-    // if (!query.exec()) {
-    //     qDebug() << "Insert failed!";
-    // } else {
-    //     qDebug() << "Insert OK!";
-    // }
-
 }
 
 MainWindow::~MainWindow()
